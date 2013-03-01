f1b69e2ecf4157fc27c93086f928c23db94a16ad
TheLuigi <jruggier@unca.edu>
2013-03-01 00:09:10 -0500

    Changes to twitterstats.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,38 +1,31 @@
 var main = function() {
 	var track1;
-	var track2;
-	var count1 = 0;
-	var count2 = 0;
+	var length = 0;
+	var count = 0; 
+	var average = 0; 
 	var twit = new ctwitter.CTwitter();
-	$("#termsButton").click(function() {
+	
+		$("#termsButton").click(function() {
 		track1 = $("#term1").val();
-		track2 = $("#term2").val();
 		$("#searchForm").hide(); 
-		$(".search1").append("<h2> Term 1: " + track1 + "</h2>" +  "<h2> Count: " + count1 + "</h2>");
-		$(".search2").append("<h2> Term 2: " + track2 + "</h2>" +  "<h2> Count: " + count2 + "</h2>");
-		
+			
+				
 		twit.stream("statuses/filter", { lang:'en', track:track1}, function(stream) {
 			stream.on("data", function(tweet) {
 				//loads tweets
-				$(".tweet").prepend("<p class='tweets'>" + tweet.text + "<p>").slideDown(100); 
-				//counts number of current tweets, maintains 10
-				$('.tweet p:gt(16)').fadeOut(1000, function() { 
+				count++;
+				length = length + (tweet.text).length; 
+				average = length/count; 
+				average = Math.round(average); 
+				$(".area").html("<h2> Term: " + track1 + "</h2>" +  "<h4> Number of tweets: " + count + "</h4>" + 
+				"<h4> Total length of tweets: " + length + "</h4>" + "<h4> Average tweet length: " + average + "</h4>");
+				$(".search").prepend("<p class='tweets'>" + tweet.text + "<p>").slideDown(100); 
+				$('.search p:gt(30)').fadeOut(1000, function() { 
 					$(this).remove() 
 				});
 			});
 		});  
-		
-		twit.stream("statuses/filter", { lang:'en', track:track2}, function(stream) {
-			stream.on("data", function(tweet) {
-				//loads tweets
-				$(".tweet").prepend("<p class='tweets'>" + tweet.text + "<p>").slideDown(100); 
-				//counts number of current tweets, maintains 10
-				$('.tweet p:gt(16)').fadeOut(1000, function() { 
-					$(this).remove() 
-				});
-			});
-		});  
-		
+				
 	});
 }
 
