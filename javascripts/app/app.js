ac70c132956cc2ea6237463f13d7fad9ba7278c8
TheLuigi <jruggier@unca.edu>
2013-02-28 22:57:48 -0500

    Sets up count.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,15 +1,15 @@
 var main = function() {
 	var track1;
 	var track2;
-	var count1;
-	var count2;
+	var count1 = 0;
+	var count2 = 0;
 	var twit = new ctwitter.CTwitter();
 	$("#termsButton").click(function() {
 		track1 = $("#term1").val();
 		track2 = $("#term2").val();
 		$("#searchForm").hide(); 
-		$(".search1").append("<h2> Term 1: " + track1 + "</h2>");
-		$(".search2").append("<h2> Term 2: " + track2 + "</h2>");
+		$(".search1").append("<h2> Term 1: " + track1 + "</h2>" +  "<h2> Count: " + count1 + "</h2>");
+		$(".search2").append("<h2> Term 2: " + track2 + "</h2>" +  "<h2> Count: " + count2 + "</h2>");
 		
 		twit.stream("statuses/filter", { lang:'en', track:track1}, function(stream) {
 			stream.on("data", function(tweet) {
